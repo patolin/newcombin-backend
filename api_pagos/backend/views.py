1a3d36d985ac937618f25490e11e7ea9963b396a
Patricio Reinoso <patricio.reinoso.mendoza@gmail.com>
2022-03-22 23:14:50 -0500

    Cambio de formato en reporte de transacciones

--- api_pagos/backend/views.py
+++ api_pagos/backend/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.db.models import Count, Sum
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -19,44 +21,29 @@ class PayablesViews(APIView):
                 serializer.save()
                 return Response({"status":"ok", "data":serializer.data}, 200)
             else:
-                return Response({"status":"error", "data":serializer.errors}, 400)
+                return Response({"status":"error", "data":serializer.errors}, 500)
         except Exception as e:
             return Response({"status":"error", "data":str(e)}, 500)
 
 
 class TransactionViews(APIView):
     def get(self, request, start_date=None, end_date=None):
-
         if (start_date and end_date):
             transactions=Transaction.objects.filter(fecha_pago__gte=start_date, fecha_pago__lte=end_date)
         else:
             transactions=Transaction.objects.all()
-
-        reporte={}
-        for transaction in transactions:
-            fecha_pago=transaction.fecha_pago.strftime('%Y-%m-%d')
-            if fecha_pago not in reporte:
-                
-                reporte[fecha_pago]={"importe_acumulado":transaction.importe_pago, "total_transacciones":1}
-            else:
-                reporte[fecha_pago]["importe_acumulado"]+=transaction.importe_pago
-                reporte[fecha_pago]["total_transacciones"]+=1
-
-            
-        #serializer=TransactionSerializer(transaction, many=True)
-        print(reporte)
-        return Response(reporte, 200)
+        transactions=transactions.values('fecha_pago').order_by('fecha_pago').annotate(cantidad_transacciones=Count('fecha_pago')).annotate(importe_acumulado=Sum('importe_pago'))
+        return Response(transactions, 200)
 
 
     def post(self, request):
         try:
             serializer = TransactionSerializer(data=request.data)
-            
             if (serializer.is_valid()):
                 serializer.save()
                 return Response({"status":"ok", "data":serializer.data}, 200)
             else:
-                return Response({"status":"error", "data":serializer.errors}, 400)
+                return Response({"status":"error", "data":serializer.errors}, 500)
         except Exception as e:
             return Response({"status":"error", "data":str(e)}, 500)
     
